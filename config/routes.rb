1e6b5a129449e1c4fc02b60305dadf813c07c7b5
YoshikaneIchimura <101252312+yoshikaneichimura@users.noreply.github.com>
2024-09-27 21:25:45 +0000

    [Add] 投稿機能のコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#'
-  get 'homes/top'
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
