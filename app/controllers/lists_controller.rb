777cb6db296ea92a6b2bcf33840fb60984b962b6
YoshikaneIchimura <101252312+yoshikaneichimura@users.noreply.github.com>
2024-10-14 22:04:39 +0000

    [Add] 投稿コントローラー 削除機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,13 +20,19 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
  
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists/' => 'lists#index'
-  get 'lists/:id' => 'lists#show',as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists
 end
