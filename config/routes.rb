48dcd165635a9e7be90adb71e962a4db009b33b9
YoshikaneIchimura <101252312+yoshikaneichimura@users.noreply.github.com>
2024-11-07 22:14:50 +0000

    [Add] 投稿画面のレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
- 
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'lists/new'
   get '/top' => 'homes#top'
   resources :lists
 end
